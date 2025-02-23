75b6c10010bfee0aa7fe7ea019e4171e7ff008e3
QuinnPaterson96 <quinnpaterson1996@gmail.com>
2025-02-23 15:28:16 -0500

    Add updated resume and modify iframe source in Resume component

--- quinnsite/src/pages/Resume.jsx
+++ quinnsite/src/pages/Resume.jsx
@@ -8,8 +8,8 @@ const Resume = () => {
             <Header/>
             <Navbar />
 
-            <iframe src="assets/QuinnPatersonResume.pdf" width="100%" height="800px">
-                <p>Your browser does not support PDFs. <a href="assets/QuinnPatersonResume.pdf" download>Download the resume here.</a></p>
+            <iframe src="assets/QuinnPatersonResume2.pdf" width="100%" height="800px">
+                <p>Your browser does not support PDFs. <a href="assets/QuinnPatersonResume2.pdf" download>Download the resume here.</a></p>
             </iframe>
         </div>
     );
