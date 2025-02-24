912f25a1d5ba9b29f725884e61ee881f96ce811d
QuinnPaterson96 <quinnpaterson1996@gmail.com>
2025-02-24 16:40:06 -0500

    noticed a potential build issue

--- quinnsite/src/pages/Resume.jsx
+++ quinnsite/src/pages/Resume.jsx
@@ -9,7 +9,7 @@ const Resume = () => {
             <Navbar />
 
             <iframe src="assets/QuinnPatersonResume.pdf" width="100%" height="800px">
-                <p>Your browser does not support PDFs. <a href="assets/QuinnPatersonResume2.pdf" download>Download the resume here.</a></p>
+                <p>Your browser does not support PDFs. <a href="assets/QuinnPatersonResume.pdf" download>Download the resume here.</a></p>
             </iframe>
         </div>
     );
