cf2e9b9627d88be11002cd2b500d80e82be068b8
QuinnPaterson96 <quinnpaterson1996@gmail.com>
2025-02-24 16:39:22 -0500

    some bugfixes

--- quinnsite/src/pages/Resume.jsx
+++ quinnsite/src/pages/Resume.jsx
@@ -8,7 +8,7 @@ const Resume = () => {
             <Header/>
             <Navbar />
 
-            <iframe src="assets/QuinnPatersonResume2.pdf" width="100%" height="800px">
+            <iframe src="assets/QuinnPatersonResume.pdf" width="100%" height="800px">
                 <p>Your browser does not support PDFs. <a href="assets/QuinnPatersonResume2.pdf" download>Download the resume here.</a></p>
             </iframe>
         </div>
